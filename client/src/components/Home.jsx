94aee057b2fd70b07fd5c47d3acc918434641c0d
bragerosberg <brage.rosberg@salt.dev>
2020-07-16 20:26:19 +0200

    App is fully styled, created APIlist to hold array

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import '../styles/Home.css';
+const apiList = require('./apiList');
 
 class Home extends React.Component {
   state = { home: [] };
@@ -11,9 +13,23 @@ class Home extends React.Component {
 
   render () {
     return (
-      <>
-        <h1>Home page</h1>
-      </>
+      <main className="homepage__wrapper">
+        <h1 className="rendered__header">Home page</h1>
+        <section className="information">
+          <section className="information__introduction">
+            <h2>Background</h2>
+            <p>Hackday project consisting of a CRA-frontend, with express backend.</p>
+            <p>Using APIs I have been curious to test out, therefore wide difference.</p>
+          </section>
+          <section className="information___api">
+            <h2>APIs used</h2>
+            <>
+              {apiList.map(api =>
+                <p key={api}>{api}</p>)}
+            </>
+          </section>
+        </section>
+      </main>
     )
   }
 }
