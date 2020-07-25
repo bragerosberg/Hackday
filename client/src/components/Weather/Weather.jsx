edfbab1f325109bc8092ac3340ffd89f05a7a910
Brage Rosberg <brage70@gmail.com>
2020-07-26 00:51:25 +0200

    Refactored Weather.jsx to use Functional Components and hooks

--- client/src/components/Weather/Weather.jsx
+++ client/src/components/Weather/Weather.jsx
@@ -1,48 +1,42 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import '../../styles/loader.css';
 import './Weather.css';
 
-class Weather extends React.Component {
-  state = { weather: [{
-    main: undefined
-    }]
-  };
+export default () => {
+  const [weather, setWeather] = useState();
+  const [fetchComplete, setFetchStatus] = useState(false);
 
-  componentDidMount() {
+  useEffect(() => {
     fetch('/weather')
       .then(res => res.json())
-      .then(weather => this.setState({weather}));
-    }
+      .then(data => {
+        setWeather(data);
+        setFetchStatus(true);
+      })
+  }, []);
 
-    render () {
-      const weather = this.state.weather;
-      if(weather.main === undefined) {
-        return (
-          <main className="loader__wrapper">
-            <h1 className="loading__header">Weather Page</h1>
-            <div className="lds-ellipsis"><div></div><div></div><div></div><div></div></div> {/* CSS loader */}
-            <p>If you experience a long load-time, please make sure the server is running</p>
-          </main>
-        )
-      }
-      return (
-        <>
-          <section className="header__wrapper">
-            <h1 className="rendered__header">Weather Page</h1>
-            <p className="rendered__subheader">An updating forecast for {weather.name}:</p>
-          </section>
-          <main className="weather__info">
-            <ul>
-              <li>Temperature: {Math.round(weather.main.temp * 10) / 10}°C</li>
-              <li>Feels Like: {Math.round(weather.main.feels_like * 10) / 10}°C</li>
-              <li>May vary between: {Math.round(weather.main.temp_min * 10) / 10}°C and {Math.round(weather.main.temp_max * 10) / 10}°C</li>
-              <li>Weather: {weather.weather[0].main}</li>
-              <li>Weather description: {weather.weather[0].description}</li>
-            </ul>
-          </main>
-        </>
-      );
-    }
+  return !fetchComplete ?
+    (
+      <main className="loader__wrapper">
+        <h1 className="loading__header">Weather Page</h1>
+        <div className="lds-ellipsis"><div></div><div></div><div></div><div></div></div> {/* CSS loader */}
+        <p>If you experience a long load-time, please make sure the server is running</p>
+      </main>
+    ) : ( 
+      <article>
+        <section className="header__wrapper">
+          <h1 className="rendered__header">Weather Page</h1>
+          <p className="rendered__subheader">An updating forecast for {weather.name}:</p>
+        </section>
+        <main className="weather__info">
+          <ul>
+            <li>Temperature: {Math.round(weather.main.temp * 10) / 10}°C</li>
+            <li>Feels Like: {Math.round(weather.main.feels_like * 10) / 10}°C</li>
+            <li>May vary between: {Math.round(weather.main.temp_min * 10) / 10}°C and {Math.round(weather.main.temp_max * 10) / 10}°C</li>
+            <li>Weather: {weather.weather[0].main}</li>
+            <li>Weather description: {weather.weather[0].description}</li>
+          </ul>
+        </main>
+      </article>
+    )
 }
-
-export default Weather;
