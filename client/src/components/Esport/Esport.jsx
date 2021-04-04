32d4cf3ba401649bc0e004c32c0b766716ea006e
Brage Rosberg <brage70@gmail.com>
2021-04-04 19:18:05 +0200

    Updated render on esport component

--- client/src/components/Esport/Esport.jsx
+++ client/src/components/Esport/Esport.jsx
@@ -17,18 +17,21 @@ export default () => {
 
   const { Country, CountryCode, NewConfirmed, TotalConfirmed } = fetchComplete ? esport : '';
 
-  return !fetchComplete ?
-    (
+  return (
+    <>
+      {!fetchComplete && (
       <div className="loader__wrapper">
         <h1 className="loading__header">E-sport Page</h1>
         <div className="lds-ellipsis"><div></div><div></div><div></div><div></div></div> {/* CSS loader */}
         <p>If you experience a long load-time, please make sure the server is running</p>
       </div>
-    ) : (
+      )}
+      {fetchComplete && (
       <div className="esport__wrapper">
         <p style={{color: "white"}}>Country: {Country}, {CountryCode}</p>
         <p style={{color: "white"}}>Total confirmed {TotalConfirmed}</p>
         <p style={{color: "white"}}>New confirmed {NewConfirmed}</p>
       </div>
-    )
-}
+      )}
+    </>
+  )}
